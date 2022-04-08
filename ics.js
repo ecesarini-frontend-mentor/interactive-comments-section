05db15ee4aae1b0818baa36433529b0dd23b2790
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-09 01:44:14 +0200

    try to init()

---
+++ ics.js
@@ -0,0 +1,8 @@
+function init() {
+    const data = fetch("./data.json").then((response) => {response.json();}); //[[PromiseResult]]: undefined. Check this
+
+    //console.log(data.then(r => console.log(r)));
+    console.log(data);
+}
+
+init();
