465aa7e20346f013d3c24af5ee14627f9290b7e1
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-12 21:58:56 +0200

    fixing style

--- ics.js
+++ ics.js
@@ -1,5 +1,5 @@
 function init() {
-    const data = fetch("./data.json").then((response) => {response.json();}); //[[PromiseResult]]: undefined. Check this
+    const data = fetch("./data.json").then((response) => {response.json();}); //[[PromiseResult]]: undefined. Check this.
 
     //console.log(data.then(r => console.log(r)));
     console.log(data);
