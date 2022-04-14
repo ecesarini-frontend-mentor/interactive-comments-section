c56239e2e277102843440c316f2cbef348d259f7
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-15 00:22:17 +0200

    TODO index.html ics.js

--- ics.js
+++ ics.js
@@ -1,8 +1,17 @@
-function init() {
-    const data = fetch("./data.json").then((response) => {response.json();}); //[[PromiseResult]]: undefined. Check this.
+// TODO: process "data.json"
 
-    //console.log(data.then(r => console.log(r)));
-    console.log(data);
+function init() {
+    //const data = fetch("./data.json").then((response) => {/*return"*/ response.json();}); // Arrow functions need return inside {}
+    //const data = async (() => fetch("./data.json").then(res => res.json()).then( data => console.log(data))();
+    let getData = async function() {
+        let data = await fetch("./data.json").then(res => res.json()).then( data => data);
+        return data;
+    }
+    //async getData();
+    
+    
+    //console.log(data);
+    return console.log(getData());
 }
 
 init();
