bd424b17bab4266d3acb4127a0e1ba4d1810454c
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-16 01:35:19 +0200

    Check out TODO:

--- ics.js
+++ ics.js
@@ -1,8 +1,49 @@
-// TODO: process "data.json"
+// TODO: style function: mouseout event doesn't change 'fill' on elements, error 'mouseout' event."
+
+
+class IcsStyle {
+    constructor(elClk, evClk) {
+        this.elClk = elClk;
+        this.evClk = evClk;
+        this.init();
+    }
+
+    init() {
+        this.elClk.forEach( el => {
+            el.addEventListener('mouseover', this);
+            el.addEventListener('mousout', this);
+            }
+        );
+            //el => el.add
+            //);
+    }
+    handleEvent(e) {
+        let ect = e.currentTarget;
+        if(ect.type === 'button') this.styleClkd(e);
+    }    
+    styleClkd(e) {
+        let ect = e.currentTarget,
+            svg = ect.querySelector('object').contentDocument.querySelector('path'),
+            svgOrigColor = ect.querySelector('object').contentDocument.querySelector('path').getAttribute('fill');
+        switch(e.type) {
+            case 'mouseover':
+                ect.style.color = 'red';
+                svg.setAttribute('fill', 'red');
+                break;
+            case 'mouseout':
+                ect.style.color = '';
+                svg.setAttribute('fill', svgOrigColor);
+                break;
+        }
+    }
+}
 
 function init() {
+    let elClk = document.querySelectorAll('button'),
+        evClk = ['mouseover', 'mouseout'];
+
+    new IcsStyle(elClk, evClk);
     //const data = fetch("./data.json").then((response) => {/*return"*/ response.json();}); // Arrow functions need return inside {}
-    //const data = async (() => fetch("./data.json").then(res => res.json()).then( data => console.log(data))();
     let getData = async function() {
         let data = await fetch("./data.json").then(res => res.json()).then( data => data);
         return data;
