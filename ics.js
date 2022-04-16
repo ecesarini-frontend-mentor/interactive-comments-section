147d8b64a7dd61088d10c13fc081b8a462e68f9b
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-17 01:29:07 +0200

    check TODO out

--- ics.js
+++ ics.js
@@ -1,4 +1,4 @@
-// TODO: style function: mouseout event doesn't change 'fill' on elements, error 'mouseout' event."
+// TODO: style function: mouseout event doesn't change 'fill' on elements, error 'mouseout' event depending on 'add-comment' button which can't refer on querySelector('object')."
 
 
 class IcsStyle {
@@ -13,32 +13,26 @@ class IcsStyle {
         this.evClk.forEach( ev => 
             this.elClk.forEach( el => el.addEventListener(ev, this) )    
         );
-        /*this.elClk.forEach( el => 
-            this.evClk.forEach( ev => el.addEventListener(ev, this) )    
-        );*/
-        /*this.elClk.forEach( el => {
-                el.addEventListener('mouseover', this);
-                el.addEventListener('mousout', this);
-                            
-            });*/ // all 3 work
         
     }
     handleEvent(e) {
         let ect = e.currentTarget,
-            svg = ect.querySelector('object').contentDocument.querySelector('path')
+            svg = ect.querySelector('object').contentDocument.querySelector('path');
+
         switch(ect.type) {
             case 'button':
-                this.styleClkd(e, svg, colorOrig);
+                this.styleClkd(e, svg);
                 break;
         }
     }    
-    styleClkd(e, svg, colorOrig) {
+    styleClkd(e, svg) {
         let ect = e.currentTarget,
-            check = (arguments.length < 3)? true : false;
+            colorOrig;
         switch(e.type) {
             case 'mouseover':
                 ect.style.color = 'red';
                 svg.setAttribute('fill', 'red');
+                colorOrig = svg.getAttribute('fill');
                 break;
             case 'mouseout':
                 ect.style.color = '';
@@ -52,17 +46,14 @@ function init() {
     let elClk = document.querySelectorAll('button'),
         evClk = ['mouseover', 'mouseout', 'mouseleave'];
 
+    window.addEventListener('DOMContenLoaded', () => sessionStorage.clear());
     new IcsStyle(elClk, evClk);
     //const data = fetch("./data.json").then((response) => {/*return"*/ response.json();}); // Arrow functions need return inside {}
-    let getData = async function() {
+    /*let getData = async function() {
         let data = await fetch("./data.json").then(res => res.json()).then( data => data);
         return data;
     }
-    //async getData();
-    
-    
-    //console.log(data);
-    return console.log(getData());
+    return console.log(getData());/*
 }
 
 init();
