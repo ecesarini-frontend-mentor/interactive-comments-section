fc900e96cb37ebbf4c5db52e8a27d140c36d8e72
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-04-20 00:02:39 +0200

    comments in 'ics.js'

--- ics.js
+++ ics.js
@@ -1,4 +1,10 @@
-// TODO: index.html TODO reference, best way?
+/* FINDINGS:
+    great design:  
+        https://github.com/Richard2957/interactive-comments-section-main/blob/master/index.html 
+        https://github.com/Richard2957/interactive-comments-section-main/blob/master/ics.js
+
+    TODO: make PopulateOnLoad depending on 'data.json'
+*/
 
 class IcsStyle {
     constructor(elClk, evClk, colorVote, colorReplyAndEdit, colorDelete) {
@@ -79,40 +85,73 @@ class IcsStyle {
 }
 
 class PopulateOnLoad {
-    constructor(data) {
-        this.data = data;
+    constructor(jFile) {
+        this.jFile = jFile;
         this.jsonData = null;
+        this.container = document.querySelector('.comment-container');
         this.init();
     }
     
-    async getData(data) {
-        this.jsonData = await fetch(data).then(res => res.json()).then( data => data);
-        //console.log(json);
-    }
     init() {
-        this.getData(this.data);
-    }
-
-    createComments() {
-        let usersHTML = document.querySelectorAll('.comment-user-main-grid-container'),
-            users = [];
-        
-        for (let i in this.jsonData.comments) {
-            users.push()
-        }
-        /*for(let elem in usersHTML) {
-            for(let i in this.jsonData.comments) {
-                elem
+        this.getData(this.jFile);
+        this.entry();
+    }    
+    async getData(jFile) {
+        this.jsonData = JSON.parse(sessionStorage.getItem(['jsonData']));
+        if(!this.jsonData) {
+            try {
+                this.jsonData = await fetch(jFile).then(res => res.json()).then( data => data);
+                sessionStorage.setItem('jsonData', JSON.stringify(this.jsonData));
+                this.drawUpDoc();
             }
-        }*/
+            catch(error) {
+                console.log(error);
+            }
+        }
     }
 
-    populate() {
-        let comments = this.jsonData.comments;
-
-        for(let i in comments) {
-
+    entry() {
+        const qualifyEntry = {
+            img(target, src) {
+                target.src = src;
+                target.setAttribute('alt', 'Avatar');
+            },
         }
+        const comment = document.createElement('div'),
+            commentUserGrid = document.createElement('div'),
+            commentRate = document.createElement('div'),
+            commentRatePlus = document.createElement('button'),
+            commentRateValue = document.createElement('span'),
+            commentRateMinus = document.createElement('button'),
+            commentTopfb = document.createElement('div'),
+            commentTopfbAvatar = new Image(),
+            commentTopfbUid = document.createElement('span'),
+            commentTopfbDate = document.createElement('span'),
+            commentTopfbBtn = document.createElement('button'),
+            commentBottomContent = document.createElement('div');
+
+        qualifyEntry.img(commentTopfbAvatar, 'images/avatars/image-amyrobson.png');
+        
+        comment.classList.add('comment-user-main-grid-container');
+        commentUserGrid.classList.add('comment-user-grid-element', 'comment-rate');
+        commentRate.classList.add('comment-rate-flexbox');
+        commentRatePlus.classList.add('comment-rate-element', 'comment-rate-element-plus');
+        commentRateValue.classList.add('comment-rate-element', 'comment-rate-element-value');
+        commentRateMinus.classList.add('comment-rate-element', 'comment-rate-element-minus');
+        commentTopfb.classList.add('comment-user-grid-element', 'comment-user-flexbox-container');
+        commentTopfbAvatar.classList.add('comment-user-flexbox-element', 'comment-avatar');
+        commentTopfbUid.classList.add('comment-user-flexbox-element', 'comment-uid');
+        commentTopfbDate.classList.add('comment-user-flexbox-element', 'comment-date');
+        commentTopfbBtn.classList.add('comment-user-flexbox-element');
+        commentBottomContent.classList.add('comment-user-grid-element', 'comment-content');
+
+        comment.append(commentUserGrid, commentTopfb, commentBottomContent);
+        commentUserGrid.append(commentRate);
+        commentRate.append(commentRatePlus, commentRateValue, commentRateMinus);
+        commentTopfb.append(commentTopfbAvatar, commentTopfbUid, commentTopfbDate, commentTopfbBtn);
+
+        this.container.append(comment);
+
     }
 }
 
