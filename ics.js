c2477b7c01f26099d1e6a295b2502b24312d729c
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-05-22 00:25:01 +0200

    TODO @ ics.js

--- ics.js
+++ ics.js
@@ -1,10 +1,11 @@
 /*  
     FIXED:
         - @icsbuildsesssion.js-->handleEvent:@191: make 'add-response-button' independent by reply or comment.
-    TODO:
         - control of the event on buildEvent: it works with reply-nested, not on first reply. Check icsbuildsession.js@168 deugging,
         - reply (//Desktop/Screenshot 2022-05-17 001102.jpg) doesn't work: add look at screenshot and add way to fit it (clicking on 'reply'  about 'remyorbson').
           look at js/icsbuildsession.js@218 to evaluate reply type.
+    TODO:
+        - manage: edit and delete: @icsbuildsession.js:@244:this.editor(appereance: https://jeth0214.github.io/FE-interactive-comments-section/);
         - handle createdAt by Date; get latest ID from jsonData; 
     
 */
