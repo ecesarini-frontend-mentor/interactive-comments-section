64189089e90fc956b88843f5d0b8d65259df216d
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-05-26 23:50:10 +0200

    check ics.js TODO

--- ics.js
+++ ics.js
@@ -1,10 +1,7 @@
 /*  
-    FIXED:
-        - reply (//Desktop/Screenshot 2022-05-17 001102.jpg) doesn't work: add look at screenshot and add way to fit it (clicking on 'reply'  about 'remyorbson').
-          look at js/icsbuildsession.js@218 to evaluate reply type.
-        - manage: edit and delete: @icsbuildsession.js:@269, after you click on reply new element is added, but it's not well-handled by eventListener. Chectk this out.
-        - once clicked 'edit', comment field is editable;
     TODO:
+        - this.json.data successfully draws up replies. Need to do the same about 'event reply'. It starts from 'this.eventsStore()'.
+            @202@208
         - need to match 'edit response' with events. Take a look to '\\DESKTOP:/get_id.jpg' to debug. Target is dybamically get id from container hosting event repsonse.
         - (appereance: https://jeth0214.github.io/FE-interactive-comments-section/);
         - handle createdAt by Date; get latest ID from jsonData; 
@@ -15,7 +12,7 @@ import {IcsStyle} from './js/icsstyle.js';
 import {BuildSession} from './js/icsbuildsession.js';
 
 function ics() {
-    document.addEventListener('DOMContentLoaded', () => localStorage.clear());
+    //document.addEventListener('DOMContentLoaded', () => localStorage.clear());
 
     let elClk = document.querySelectorAll('button'),
         evClk = ['mouseover', 'mouseout', 'mousedown', 'mouseup'],
