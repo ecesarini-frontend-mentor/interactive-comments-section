5631c109a8360b4986d44adf41b5b3d29dda1e54
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-06-26 01:10:31 +0200

    check @ TODO

--- js/icsmq.js
+++ js/icsmq.js
@@ -1,7 +1,19 @@
-export class IcsMq {
+/*export class IcsMq {
     constructor() {}
 
     rearrangeCommentContainer() {
-         debugger;
+        const cumgcTail = document.createElement('div');
+        let cumgc = document.querySelectorAll('.comment-user-main-grid-container');
+
+        cumgcTail.classList.add('comment-user-tail-container');
+        debugger;
+        cumgc.forEach(c => {
+            let cScore = c.querySelector('.comment-rate'),
+                cFbBtn = c.querySelectorAll('.comment-user-flexbox-container button');
+
+            c.append(cumgcTail);
+            cumgcTail.append(cScore);
+            cFbBtn.forEach(cfb => cumgcTail.append(cfb));
+        });
     }
-}
+}*/
